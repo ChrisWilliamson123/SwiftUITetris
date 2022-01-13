f40d79ad8be2b20f70b5f1f3afa851cce73785ef
Chris Williamson <cjwilliamson109@gmail.com>
2022-01-13 22:01:33 +0000

    Got a very basic game working with the ability to move pieces left and right.

--- SwiftUITetris/ContentView.swift
+++ SwiftUITetris/ContentView.swift
@@ -8,9 +8,141 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var board: BoardView.Board = BoardView.Board()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    @State private var gameIsOver = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if gameIsOver {
+            Text("Game Over!").font(.largeTitle).foregroundColor(.red)
+            Button("New Game") {
+                gameIsOver = false
+                counter = 0
+                board = BoardView.Board()
+            }
+        } else {
+            VStack {
+                BoardView(board: board)
+                    .padding()
+                    .onReceive(timer) { _ in
+                        assignNextState(timerValue: counter)
+                        counter += 1
+                    }
+                HStack(spacing: 100) {
+                    Button {
+                        movePieceLeft()
+                    } label: {
+                        Image(systemName: "arrow.backward.circle.fill").resizable().frame(width: 44, height: 44)
+                    }
+                    Button {
+                        movePieceRight()
+                    } label: {
+                        Image(systemName: "arrow.forward.circle.fill").resizable().frame(width: 44, height: 44)
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    private func movePieceRight() {
+        guard let latestPiece = board.inPlayBlocks.last else { return }
+        print(latestPiece)
+        let coordsOfOtherBlocks = Set(board.inPlayBlocks.map({ $0.coords }).flatMap({ $0 }))
+        let coordsRightOf: [Coordinate] = latestPiece.coords.compactMap({ coord in
+            let right = Coordinate(coord.x+1, coord.y)
+            if latestPiece.coords.contains(right) { return nil }
+            return right
+        })
+        
+        print(coordsRightOf)
+        
+        for rightCoord in coordsRightOf {
+            if coordsOfOtherBlocks.contains(rightCoord) || rightCoord.x >= 10 {
+                return
+            }
+        }
+        
+        var newBlocks = board.inPlayBlocks.filter({ $0 != latestPiece })
+        newBlocks.append(.init(block: latestPiece.block, coords: latestPiece.coords.reduce(into: [], { $0.insert(.init($1.x+1, $1.y)) })))
+        
+        board = BoardView.Board(inPlayBlocks: newBlocks)
+    }
+    
+    private func movePieceLeft() {
+        guard let latestPiece = board.inPlayBlocks.last else { return }
+        print(latestPiece)
+        let coordsOfOtherBlocks = Set(board.inPlayBlocks.map({ $0.coords }).flatMap({ $0 }))
+        let coordsLeftOf: [Coordinate] = latestPiece.coords.compactMap({ coord in
+            let left = Coordinate(coord.x-1, coord.y)
+            if latestPiece.coords.contains(left) { return nil }
+            return left
+        })
+        
+        print(coordsLeftOf)
+        
+        for leftCoord in coordsLeftOf {
+            if coordsOfOtherBlocks.contains(leftCoord) || leftCoord.x < 0 {
+                return
+            }
+        }
+        
+        var newBlocks = board.inPlayBlocks.filter({ $0 != latestPiece })
+        newBlocks.append(.init(block: latestPiece.block, coords: latestPiece.coords.reduce(into: [], { $0.insert(.init($1.x-1, $1.y)) })))
+        
+        board = BoardView.Board(inPlayBlocks: newBlocks)
+    }
+    
+    private func assignNextState(timerValue: Int) {
+        let oldInPlayBlocks = board.inPlayBlocks
+        var newInPlayBlocks: [BoardView.InPlayBlock] = []
+        
+        // Move existing pieces down one
+        for block in oldInPlayBlocks {
+            let shouldMoveDown = shouldMoveBlockDown(block: block, otherBlocks: Set(oldInPlayBlocks))
+            if shouldMoveDown {
+                newInPlayBlocks.append(.init(block: block.block, coords: block.coords.reduce(into: [], { $0.insert(.init($1.x, $1.y+1)) })))
+            } else {
+                newInPlayBlocks.append(block)
+            }
+        }
+        
+        // Spawn a new piece if nothing moved
+        if newInPlayBlocks == oldInPlayBlocks {
+            let block = BoardView.Block.allCases.shuffled().first!
+            // We need to check if we can spawn the block
+            let allNewBlocks = newInPlayBlocks.map({ $0.coords }).flatMap({ $0 })
+            for coord in block.spawnCoordinates {
+                if allNewBlocks.contains(coord) { gameIsOver = true; return }
+            }
+            newInPlayBlocks.append(.init(block: block, coords: block.spawnCoordinates))
+        }
+        
+        
+        
+        board = BoardView.Board(inPlayBlocks: newInPlayBlocks)
+    }
+    
+    private func shouldMoveBlockDown(block: BoardView.InPlayBlock, otherBlocks: Set<BoardView.InPlayBlock>) -> Bool {
+        let coordsOfOtherBlocks = Set(otherBlocks.map({ $0.coords }).flatMap({ $0 }))
+        let coordsUnderPiece: [Coordinate] = block.coords.compactMap({ coord in
+            let under = Coordinate(coord.x, coord.y + 1)
+            if block.coords.contains(under) { return nil }
+            return under
+        })
+//        print(coordsUnderPiece)
+        
+        // If all of the blocks under the bottom blocks are empty, we can move the piece
+        for underCoord in coordsUnderPiece {
+            if underCoord.y == 20 { return false }
+            if coordsOfOtherBlocks.contains(underCoord) {
+                return false
+            }
+        }
+        
+        return true
     }
 }
 
--- SwiftUITetris/Views/Board/Board.swift
+++ SwiftUITetris/Views/Board/Board.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct BoardView: View {
     let board: Board
     
+    init(board: Board = Board()) {
+        self.board = board
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -45,15 +49,23 @@ struct BoardView: View {
     
     struct Board {
         let data: [[BlockColour?]]
+        let inPlayBlocks: [InPlayBlock]
+//        let settledBlocks: Set<InPlayBlock>
         
-        init(data: [[BlockColour?]]? = nil) {
-            if let data = data {
-                self.data = data
-                return
+        static let allNilData: [[BlockColour?]] = Array(repeating: Array(repeating: nil, count: 10), count: 20)
+        
+        init(inPlayBlocks: [InPlayBlock] = []) {
+            self.inPlayBlocks = inPlayBlocks
+            
+            var data: [[BlockColour?]] = Self.allNilData
+            
+            for ipb in inPlayBlocks {
+                for c in ipb.coords {
+                    data[c.y][c.x] = ipb.block.colour
+                }
             }
             
-            // Initialise data to be all clear
-            self.data = Array(repeating: Array(repeating: nil, count: 10), count: 20)
+            self.data = data
         }
     }
     
@@ -79,40 +91,76 @@ struct BoardView: View {
         }
     }
     
-    struct Coordinate {
-        let x: Int
-        let y: Int
+    enum Block: CaseIterable {
+        case tBlock
+        case iBlock
+        case zBlock
+        case sBlock
+        case oBlock
+        case lBlock
+        case jBlock
         
-        init(_ x: Int, _ y: Int) {
-            self.x = x
-            self.y = y
+        var colour: BlockColour {
+            switch self {
+            case .iBlock: return .lightBlue
+            case .jBlock: return .darkBlue
+            case .lBlock: return .orange
+            case .oBlock: return .yellow
+            case .sBlock: return .green
+            case .tBlock: return .purple
+            case .zBlock: return .red
+            }
+        }
+        
+        var spawnCoordinates: Set<Coordinate> {
+            switch self {
+            case .tBlock: return [.init(4, 0), .init(3, 1), .init(4, 1), .init(5, 1)]
+            case .iBlock: return [.init(3, 1), .init(4, 1), .init(5, 1), .init(6, 1)]
+            case .zBlock: return [.init(3, 0), .init(4, 0), .init(4, 1), .init(5, 1)]
+            case .sBlock: return [.init(4, 0), .init(5, 0), .init(3, 1), .init(4, 1)]
+            case .oBlock: return [.init(4, 0), .init(5, 0), .init(4, 1), .init(5, 1)]
+            case .lBlock: return [.init(5, 0), .init(3, 1), .init(4, 1), .init(5, 1)]
+            case .jBlock: return [.init(3, 0), .init(3, 1), .init(4, 1), .init(5, 1)]
+            }
         }
     }
     
-    struct BoardView_Previews: PreviewProvider {
-        static var previews: some View {
-            BoardView(board: Board(data: [
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
-                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
-                [.red, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue]
-            ])).padding()
-        }
+    struct InPlayBlock: Hashable {
+        let block: Block
+        let coords: Set<Coordinate>
+    }
+    
+//    struct BoardView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            ScrollView {
+//                VStack {
+//                    
+//                    ForEach(Block.allCases, id: \.self) { block in
+//                        buildBoardWithColouredBlocks(coords: block.spawnCoordinates, colour: block.colour)
+//                            .padding()
+//                            .frame(width: 300, height: 700)
+//                    }
+//                }
+//            }
+//        }
+//        
+//        private static func buildBoardWithColouredBlocks(coords: Set<Coordinate>, colour: BlockColour) -> BoardView {
+//            var data = Board.allNilData
+//            for c in coords {
+//                data[c.y][c.x] = colour
+//            }
+//            
+//            return BoardView(board: Board(data: data))
+//        }
+//    }
+}
+
+struct Coordinate: Hashable {
+    let x: Int
+    let y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
     }
 }
