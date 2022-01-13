db065339b4ac59d214e53bac6833a304c9d44736
Chris <chris@192.168.1.211>
2022-01-13 19:58:57 +0000

    Added a data structure to the board.

--- SwiftUITetris/Views/Board/Board.swift
+++ SwiftUITetris/Views/Board/Board.swift
@@ -7,44 +7,112 @@
 
 import SwiftUI
 
-struct Board: View {
+struct BoardView: View {
+    let board: Board
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                VStack {
+                VStack(spacing: 0) {
                     HStack(spacing: 0) {
                         ForEach(0..<12) { _ in
                             Cube.grey
                         }
                     }.frame(height: geometry.size.width / 12)
-                    Spacer()
+                    ForEach(0..<20) { y in
+                        HStack(spacing: 0) {
+                            Cube.grey
+                            ForEach(0..<10) { x in
+                                if let colour = board.data[y][x] {
+                                    colour.cube
+                                } else {
+                                    Rectangle().fill(.clear)
+                                }
+                            }
+                            Cube.grey
+                        }.frame(height: geometry.size.width / 12)
+                    }
                     HStack(spacing: 0) {
                         ForEach(0..<12) { _ in
                             Cube.grey
                         }
                     }.frame(height: geometry.size.width / 12)
                 }
-                HStack {
-                    VStack(spacing: 0) {
-                        ForEach(0..<22) { _ in
-                            Cube.grey
-                        }
-                    }.frame(width: geometry.size.width / 12)
-                    Spacer()
-                    VStack(spacing: 0) {
-                        ForEach(0..<22) { _ in
-                            Cube.grey
-                        }
-                    }.frame(width: geometry.size.width / 12)
-                }
+                .background(.black)
             }
-            .background(.black)
         }
     }
-}
-
-struct Board_Previews: PreviewProvider {
-    static var previews: some View {
-        Board().padding()
+    
+    struct Board {
+        let data: [[BlockColour?]]
+        
+        init(data: [[BlockColour?]]? = nil) {
+            if let data = data {
+                self.data = data
+                return
+            }
+            
+            // Initialise data to be all clear
+            self.data = Array(repeating: Array(repeating: nil, count: 10), count: 20)
+        }
+    }
+    
+    enum BlockColour {
+        case lightBlue
+        case darkBlue
+        case orange
+        case yellow
+        case green
+        case purple
+        case red
+        
+        var cube: Cube {
+            switch self {
+            case .lightBlue: return Cube.lightBlue
+            case .darkBlue: return Cube.darkBlue
+            case .orange: return Cube.orange
+            case .yellow: return Cube.yellow
+            case .green: return Cube.green
+            case .purple: return Cube.purple
+            case .red: return Cube.red
+            }
+        }
+    }
+    
+    struct Coordinate {
+        let x: Int
+        let y: Int
+        
+        init(_ x: Int, _ y: Int) {
+            self.x = x
+            self.y = y
+        }
+    }
+    
+    struct BoardView_Previews: PreviewProvider {
+        static var previews: some View {
+            BoardView(board: Board(data: [
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
+                [nil, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue],
+                [.red, nil, nil, nil, nil, nil, nil, nil, nil, .lightBlue]
+            ])).padding()
+        }
     }
 }
