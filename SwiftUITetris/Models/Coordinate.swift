340fae9472402cb82599069b4e34be643fb12e72
Chris Williamson <cjwilliamson109@gmail.com>
2022-01-16 20:31:42 +0000

    Removed unused coord rotation function.

--- SwiftUITetris/Models/Coordinate.swift
+++ SwiftUITetris/Models/Coordinate.swift
@@ -24,19 +24,4 @@ struct Coordinate: Hashable {
         print(self, origin, Coordinate(Int(round(newX)), Int(round(newY))))
         return Coordinate(Int(round(newX)), Int(round(newY)))
     }
-    
-    func rotatingAround(origin: (x: Double, y: Double), by degrees: CGFloat) -> Coordinate {
-        let dx = Double(x) - origin.x
-        let dy = Double(y) - origin.y
-        print(dx, dy)
-        let radius = sqrt(dx * dx + dy * dy)
-        print(radius)
-        let azimuth = atan2(dy, dx) // in radians
-        let newAzimuth = azimuth + degrees * .pi / 180 // convert it to radians
-        let newX = Double(origin.x) + radius * cos(newAzimuth)
-        let newY = Double(origin.y) + radius * sin(newAzimuth)
-        print(newX, newY)
-        print(self, origin, Coordinate(Int(round(newX)), Int(round(newY))))
-        return Coordinate(Int(round(newX)), Int(round(newY)))
-    }
 }
